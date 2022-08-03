bd1675c7e2ac159e6da6506de2dc52c41c5fdc8f
greencrimea <tom.horton100@gmail.com>
2022-08-03 17:51:08 +1000

    .

--- main.py
+++ main.py
@@ -558,5 +558,14 @@ def get_mempool():
     response = {"mempool": blockchain.contracts}
     return jsonify(response), 200
 
+@app.route("/")
+def serve_index():
+    return render_template('index.html')
 
-app.run (host="0.0.0.0", port=5000)      # change port to run multiple instances on a single machine for development
+
+@app.route("/test.html")
+def serve_test():
+    return render_template('test.html')
+
+
+app.run (host="0.0.0.0", port=5000, debug=True)      # change port to run multiple instances on a single machine for development
